d5fb8e9db60dd421ba1d9568f09b9de9e47ec9d2
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-01-30 21:16:43 -0300

    pagina inicial

---
+++ App/View/paginaInicial.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifólio - Games</title>
+    <link rel="stylesheet" href="../../Assets/css/paginaInicial.css">
+</head>
+
+<body>
+    <section class="Primeira-Parte">
+        <div class="computador">
+            <header>
+            <!-- <div class="Borda-Colorida"> -->
+                <nav>
+                        <a href="https://github.com/Profissional-Marcelo"><img class="icones" src="https://img.icons8.com/nolan/64/github.png" alt="github"></a>
+                        <a href=""><img class="icones" src="https://img.icons8.com/nolan/64/open-resume.png" alt="curriculo" id="curriculo"></a>
+                        <a href=""><img class="icones" src="https://img.icons8.com/nolan/64/linkedin.png" alt="linkedin"></a>
+                        <a href=""><img src="https://img.icons8.com/nolan/64/instagram-new.png" alt="instagram"></a>
+                </nav>
+            <!-- </div> -->
+            </header>
+            <div class="Horizontal-Fim">
+                <button><img src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/64/external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26" class="desligado"></button>
+                <button><img src="https://img.icons8.com/external-icongeek26-outline-icongeek26/64/external-lamp-retro-80s-icongeek26-outline-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-icongeek26" class="ligado"/></button>
+            </div>
+        </div>
+        <section class="texto">
+            <p>Olá, eu sou Marcelo!</p>
+            <h1>Game</h1>
+            <a href="#"><img src="../../Assets/img/console.png" alt="jogos"></a>
+            <h1>Developer</h1>
+            <p>tenho 20 anos e atuo como desenvolvedor de jogos e web</p>
+        </section>
+    </section>
+
+    <script src=""></script>
+</body>
+
+</html>
