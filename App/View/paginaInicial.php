e694f10a61b9bb976836acb7735a7dd5187ea4f1
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-01-31 21:06:06 -0300

    terminando a sessão 1

--- App/View/paginaInicial.php
+++ App/View/paginaInicial.php
@@ -6,24 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portifólio - Games</title>
     <link rel="stylesheet" href="../../Assets/css/paginaInicial.css">
+    <link rel="stylesheet" href="../../Assets/css/paginaInicialMQ.css">
 </head>
 
 <body>
     <section class="Primeira-Parte">
         <div class="computador">
             <header>
-            <div>
+            <div id="Borda-Colorida">
                 <nav>
-                        <a href="https://github.com/Profissional-Marcelo"><img class="icones" src="https://img.icons8.com/nolan/64/github.png" alt="github"></a>
-                        <a href=""><img class="icones" src="https://img.icons8.com/nolan/64/open-resume.png" alt="curriculo" id="curriculo"></a>
-                        <a href=""><img class="icones" src="https://img.icons8.com/nolan/64/linkedin.png" alt="linkedin"></a>
-                        <a href=""><img src="https://img.icons8.com/nolan/64/instagram-new.png" alt="instagram"></a>
+                        <a href="https://github.com/Profissional-Marcelo" class="Icones-Modo-Escuro"><img class="icones" src="https://img.icons8.com/nolan/256/github.png" alt="github"/></a>
+                        <a href="" class="Icones-Modo-Escuro"><img class="icones" src="https://img.icons8.com/nolan/256/open-resume.png" alt="curriculo" id="curriculo"></a>
+                        <a href="" class="Icones-Modo-Escuro"><img class="icones" src="https://img.icons8.com/nolan/256/linkedin.png" alt="linkedin"></a>
+                        <a href="" class="Icones-Modo-Escuro"><img src="https://img.icons8.com/nolan/256/instagram-new.png" alt="instagram"></a>
+                        <!-- Icones no modo claro -->
+                        <a href="" class="Icones-Modo-Claro"><img id="githubMC" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/96/external-github-community-for-software-building-and-testing-online-logo-shadow-tal-revivo.png" alt="external-github-community-for-software-building-and-testing-online-logo-shadow-tal-revivo"></a>
+                        <a href="" class="Icones-Modo-Claro"><img id="curriculoMC" src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/512/external-curriculum-management-kiranshastry-solid-kiranshastry-1.png" alt="external-curriculum-management-kiranshastry-solid-kiranshastry-1"></a>
+                        <a href="" class="Icones-Modo-Claro"><img id="linkedinMC" src="https://img.icons8.com/glyph-neue/256/linkedin.png" alt="linkedin"></a>
+                        <a href="" class="Icones-Modo-Claro"><img id="instagramMC" src="https://img.icons8.com/glyph-neue/256/instagram-new--v1.png" alt="instagram"></a>
                 </nav>
             </div> 
             </header>
             <div class="Horizontal-Fim">
-                <button><img src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/64/external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26" class="desligado" id="desligado"></button>
-                <button><img src="https://img.icons8.com/external-icongeek26-outline-icongeek26/64/external-lamp-retro-80s-icongeek26-outline-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-icongeek26" class="ligado" id="ligado"/></button>
+                <button><img src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/256/external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26" class="desligado" id="desligado"></button>
+                <button><img src="https://img.icons8.com/external-icongeek26-outline-icongeek26/256/external-lamp-retro-80s-icongeek26-outline-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-icongeek26" class="ligado" id="ligado"/></button>
             </div>
         </div>
         <section class="texto">
@@ -35,8 +41,10 @@
             <p>tenho 20 anos e atuo como desenvolvedor de jogos e web</p>
         </section>
     </section>
-
+    <!-- <a href="https://www.marcelorei.com.br" class="Botao-Home"><img src="https://img.icons8.com/glyph-neue/64/home--v1.png" alt="home--v1"/></a>
     <script src="../../Assets/js/paginaInicial.js"></script>
+    <a href="https://www.marcelorei.com.br" class="Botao-HomeMC"><img src="https://img.icons8.com/glyph-neue/64/home--v1.png" alt="home--v1"/></a>  -->
+    <script src="../../Assets/js/paginaInicial.js"></script> 
 </body>
 
 </html>
--- Assets/js/paginaInicial.js
+++ Assets/js/paginaInicial.js
@@ -2,12 +2,24 @@
 // Carregamento do DOM
 document.addEventListener("DOMContentLoaded", function() {
     // Váriaveis
-    desligado = document.getElementById("desligado");
-    ligado = document.getElementById("ligado");
+    let desligado = document.getElementById("desligado");
+    let ligado = document.getElementById("ligado");
 
     // Eventos
     desligado.addEventListener("click", modoClaro);
     ligado.addEventListener("click", modoClaro);
+
+    const nav = document.querySelector("nav");
+    // Adicionando evento de mouse enter e mouse leave ao botão
+    nav.addEventListener("mouseenter", function() {
+        const BordaColorida = document.querySelector("#Borda-Colorida");   
+        BordaColorida.classList.add("Borda-Colorida");
+    })
+
+    nav.addEventListener("mouseleave", function() {
+        const BordaColorida = document.querySelector("#Borda-Colorida");   
+        BordaColorida.classList.remove("Borda-Colorida");
+    })
 
     // Funções
 
@@ -21,6 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
         const console = document.querySelector("#console");
         const consoleMC = document.querySelector("#Console-M-C");
         const nav = document.querySelector("nav");
+        const iconesME = document.querySelectorAll(".Icones-Modo-Escuro");
+        const iconesMC = document.querySelectorAll(".Icones-Modo-Claro");
+        // const botaoHome = document.querySelector(".Botao-Home");
+        // const botaoHomeMC = document.querySelector(".Botao-HomeMC");
 
         if (ligadoDisplay === "none") {
             ligado.style.display = "block";
@@ -28,14 +44,20 @@ document.addEventListener("DOMContentLoaded", function() {
             sectionPrimeiraParte.classList.add("fundoClaro");
             console.style.display = "none";
             consoleMC.style.display = "inline-block";
-            
             nav.style.backgroundColor = "#CCCCCA";
-            
-            h1.forEach(element => {
-                element.classList.add("Letra-MClaro");
+            // botaoHome.style.display = "none";
+            // botaoHomeMC.style.display = "inline-block";
+            iconesME.forEach(elementoIconesME => {
+                elementoIconesME.style.display = "none";
             });
-            p.forEach(element => {
-                element.classList.add("Letra-MClaro");
+            iconesMC.forEach(elementoIconesMC => {
+                elementoIconesMC.style.display = "inline-block";
+            });
+            h1.forEach(elementoh1 => {
+                elementoh1.classList.add("Letra-MClaro");
+            });
+            p.forEach(elementop => {
+                elementop.classList.add("Letra-MClaro");
             });
         } else {
             ligado.style.display = "none";
@@ -43,12 +65,21 @@ document.addEventListener("DOMContentLoaded", function() {
             sectionPrimeiraParte.classList.remove("fundoClaro");
             console.style.display = "inline-block";
             consoleMC.style.display = "none";
-            h1.forEach(element => {
-                element.classList.remove("Letra-MClaro");
+            nav.style.backgroundColor = "#363232";
+            // botaoHome.style.display = "inline-block";
+            // botaoHomeMC.style.display = "none";
+            iconesME.forEach(elementoIconesME => {
+                elementoIconesME.style.display = "inline-block";
+            });
+            iconesMC.forEach(elementoIconesMC => {
+                elementoIconesMC.style.display = "none";
+            });
+            h1.forEach(elementoh1 => {
+                elementoh1.classList.remove("Letra-MClaro");
             });
     
-            p.forEach(element => {
-                element.classList.remove("Letra-MClaro");
+            p.forEach(elementop => {
+                elementop.classList.remove("Letra-MClaro");
             });
         }
     }
