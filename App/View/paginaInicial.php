73d8f24d1437178febcd4a83fb2daa7f5a829cea
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-08-09 14:21:12 -0300

    refazendo header

--- App/View/paginaInicial.php
+++ App/View/paginaInicial.php
@@ -6,17 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portifólio - Games</title>
     <meta name="description" content="Portifólio do desenvolvedor de games e web full stack, Marcelo Henrique Cornelis Rei.">
+    <link rel="stylesheet" href="../../Assets/css/header.css">
+    <link rel="stylesheet" href="../../Assets/css/headerMQ.css">
     <link rel="stylesheet" href="../../Assets/css/paginaInicial.css">
     <link rel="stylesheet" href="../../Assets/css/paginaInicialMQ.css">
 </head>
 
 <body>
     <section class="Primeira-Parte">
-        <div class="computador">
-            <header>
-            <div id="Borda-Colorida">
-                <nav>
-                        <a href="https://github.com/Profissional-Marcelo" class="Icones-Modo-Escuro" target="_blank"><img class="icones" src="https://img.icons8.com/nolan/256/github.png" alt="github"/></a>
+        <header>
+            <nav>
+                <div id="Borda-Colorida">
+                    <menu>
+                        <a href="https://github.com/Profissional-Marcelo" class="Icones-Modo-Escuro" target="_blank"><img class="icones" src="https://img.icons8.com/nolan/256/github.png" alt="github" /></a>
                         <a href="" class="Icones-Modo-Escuro" target="_blank"><img class="icones" src="https://img.icons8.com/nolan/256/open-resume.png" alt="curriculo" id="curriculo"></a>
                         <a href="" class="Icones-Modo-Escuro" target="_blank"><img class="icones" src="https://img.icons8.com/nolan/256/linkedin.png" alt="linkedin"></a>
                         <a href="" class="Icones-Modo-Escuro" target="_blank"><img src="https://img.icons8.com/nolan/256/instagram-new.png" alt="instagram"></a>
@@ -25,14 +27,15 @@
                         <a href="" class="Icones-Modo-Claro" target="_blank"><img id="curriculoMC" src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/512/external-curriculum-management-kiranshastry-solid-kiranshastry-1.png" alt="external-curriculum-management-kiranshastry-solid-kiranshastry-1"></a>
                         <a href="" class="Icones-Modo-Claro" target="_blank"><img id="linkedinMC" src="https://img.icons8.com/glyph-neue/256/linkedin.png" alt="linkedin"></a>
                         <a href="" class="Icones-Modo-Claro" target="_blank"><img id="instagramMC" src="https://img.icons8.com/glyph-neue/256/instagram-new--v1.png" alt="instagram"></a>
-                </nav>
-            </div> 
-            </header>
-            <div class="Horizontal-Fim">
-                <button><img src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/256/external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26" class="desligado" id="desligado"></button>
-                <button><img src="https://img.icons8.com/external-icongeek26-outline-icongeek26/256/external-lamp-retro-80s-icongeek26-outline-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-icongeek26" class="ligado" id="ligado"/></button>
+                    </menu>
+                </div>
+            </nav>
+
+            <div class="lampada">
+                <button class="btn-desligado"><img src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/256/external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-gradient-icongeek26" class="desligado" id="desligado"></button>
+                <button class="btn-ligado"><img src="https://img.icons8.com/external-icongeek26-outline-icongeek26/256/external-lamp-retro-80s-icongeek26-outline-icongeek26.png" alt="external-lamp-retro-80s-icongeek26-outline-icongeek26" class="ligado" id="ligado" /></button>
             </div>
-        </div>
+        </header>
         <section class="texto">
             <p>Olá, eu sou Marcelo!</p>
             <h1>Game</h1>
@@ -46,13 +49,13 @@
         <div class="texto">
             <h1>Plataformas que utilizo</h1>
             <p>Pressione sobre a plataforma para visualizar os projetos!</p>
-            <img src="https://img.icons8.com/doodle/96/oil-lamp.png" alt="oil-lamp"/>
+            <img src="https://img.icons8.com/doodle/96/oil-lamp.png" alt="oil-lamp" />
         </div>
         <div class="plataformas">
             <div class="border-color">
                 <div class="plataforma-scratch borda1">
                     <a href="#">
-                    <img src="https://img.icons8.com/nolan/512/scratch.png" alt="scratch"/>
+                        <img src="https://img.icons8.com/nolan/512/scratch.png" alt="scratch" />
                     </a>
                     <p>SCRATCH</p>
                 </div>
@@ -60,7 +63,7 @@
             <div class="border-color2">
                 <div class="plataforma-unity borda2">
                     <a href="#">
-                    <img src="https://img.icons8.com/nolan/512/unity.png" alt="unity"/>
+                        <img src="https://img.icons8.com/nolan/512/unity.png" alt="unity" />
                     </a>
                     <p>UNITY</p>
                 </div>
@@ -68,7 +71,7 @@
             <div class="border-color3">
                 <div class="plataforma-java borda3">
                     <a href="#">
-                    <img src="https://img.icons8.com/nolan/512/java-coffee-cup-logo.png" alt="java-coffee-cup-logo"/>
+                        <img src="https://img.icons8.com/nolan/512/java-coffee-cup-logo.png" alt="java-coffee-cup-logo" />
                     </a>
                     <p>Java</p>
                 </div>
@@ -76,7 +79,7 @@
             <div class="border-color4">
                 <div class="plataforma-roblox borda4">
                     <a href="#">
-                    <img src="https://img.icons8.com/nolan/512/roblox.png" alt="roblox"/>
+                        <img src="https://img.icons8.com/nolan/512/roblox.png" alt="roblox" />
                     </a>
                     <p>Roblox</p>
                 </div>
@@ -84,7 +87,7 @@
             <div class="border-color5">
                 <div class="plataforma-javascript borda5">
                     <a href="#">
-                    <img src="https://img.icons8.com/nolan/512/javascript-logo.png" alt="javascript-logo"/>
+                        <img src="https://img.icons8.com/nolan/512/javascript-logo.png" alt="javascript-logo" />
                     </a>
                     <p>JavaScript</p>
                 </div>
@@ -94,7 +97,7 @@
     <!-- <a href="https://www.marcelorei.com.br" class="Botao-Home"><img src="https://img.icons8.com/glyph-neue/64/home--v1.png" alt="home--v1"/></a>
     <script src="../../Assets/js/paginaInicial.js"></script>
     <a href="https://www.marcelorei.com.br" class="Botao-HomeMC"><img src="https://img.icons8.com/glyph-neue/64/home--v1.png" alt="home--v1"/></a>  -->
-    <script src="../../Assets/js/paginaInicial.js"></script> 
+    <script src="../../Assets/js/paginaInicial.js"></script>
 </body>
 
 </html>
